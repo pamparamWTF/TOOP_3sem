bdee79f896b509dc9ac2ccd2b3b5681e5a86a232
SpirinAS <pamparamwtf@gmail.com>
2023-09-27 14:27:54 +0700

    Реализовал l2 функционал с горадиентом, модифицировал оптимизатор МонтеКарло

--- TOOP_3sem/Program.cs
+++ TOOP_3sem/Program.cs
@@ -6,17 +6,14 @@ using System.Runtime.Intrinsics;
 namespace TOOP_3sem
 {
     interface IVector : IList<double> { }
-
     interface IParametricFunction
     {
         IFunction Bind(IVector parameters);
     }
-
     interface IFunction
     {
         double Value(IVector point);
     }
-
     interface IDifferentiableFunction : IFunction
     {
         // По параметрам исходной IParametricFunction
@@ -55,21 +52,21 @@ namespace TOOP_3sem
         }
         public IFunction Bind(IVector parameters) => new InternalLineFunction() { a = parameters[0], b = parameters[1] };
     }
-    class MyPolynom : IParametricFunction
+    class MyFunctional : IFunctional
     {
-        class InternalLineFunction : IFunction
+        public List<(double x, double y)> points;
+        public double Value(IFunction function)
         {
-            public IVector a;
-            public double Value(IVector point) 
+            double sum = 0;
+            foreach (var point in points)
             {
-                double sum = 0;
-                for (int i = a.Count - 1; i >= 0; i--)
-                    sum += a[i] * Math.Pow(point[0], i);
-                return sum;
+                var param = new Vector();
+                param.Add(point.x);
+                var s = function.Value(param) - point.y;
+                sum += s * s;
             }
+            return sum;
         }
-        public IFunction Bind(IVector parameters) => new InternalLineFunction() { a = parameters };
-
     }
     class MyLineNFunction : IParametricFunction
     {
@@ -90,7 +87,7 @@ namespace TOOP_3sem
             {
                 IVector grad = new Vector();
 
-                for(int i = 0; i < point.Count; i++)
+                for (int i = 0; i < point.Count; i++)
                     grad.Add(point[i]);
                 grad.Add(0);
                 return grad;
@@ -99,23 +96,23 @@ namespace TOOP_3sem
         public IFunction Bind(IVector parameters) => new InternalLineFunction() { a = parameters };
 
     }
-    class MyFunctional : IFunctional
+    class MyPolynom : IParametricFunction
     {
-        public List<(double x, double y)> points;
-        public double Value(IFunction function)
+        class InternalLineFunction : IFunction
         {
-            double sum = 0;
-            foreach (var point in points)
+            public IVector a;
+            public double Value(IVector point)
             {
-                var param = new Vector();
-                param.Add(point.x);
-                var s = function.Value(param) - point.y;
-                sum += s * s;
+                double sum = 0;
+                for (int i = a.Count - 1; i >= 0; i--)
+                    sum += a[i] * Math.Pow(point[0], i);
+                return sum;
             }
-            return sum;
         }
+        public IFunction Bind(IVector parameters) => new InternalLineFunction() { a = parameters };
+
     }
-    class MyL1NormDifferentiableFunctional : IDifferentiableFunctional
+    class MyL1Functional : IDifferentiableFunctional
     {
         public List<(IVector point, double f)> pointsAndF;
         public double Value(IFunction function)
@@ -135,14 +132,53 @@ namespace TOOP_3sem
 
                 fun = function as IDifferentiableFunction;
 
-                for (int i = 0; i <= pointsAndF[0].point.Count; i++)
-                    grad.Add(1);
+                for (int i = 0; i < pointsAndF[0].point.Count; i++)
+                    grad.Add(0);
+                grad.Add(1);
 
                 for (int i = 0; i < pointsAndF.Count; i++)
                 {
                     sign = Math.Sign(function.Value(pointsAndF[i].point) - pointsAndF[i].f);
                     for (int j = 0; j <= pointsAndF[i].point.Count; j++)
                         grad[j] += sign * fun.Gradient(pointsAndF[i].point)[j];
+                }
+                return grad;
+            }
+            else
+            {
+                Console.WriteLine("ERROR!!! This Function is not Differentiable!");
+                return grad;
+            }
+        }
+    }
+    class MyL2Functional : IDifferentiableFunctional
+    {
+        public List<(IVector point, double f)> pointsAndF;
+        public double Value(IFunction function)
+        {
+            double sum = 0, s = 0;
+            foreach (var pointAndF in pointsAndF)
+            {
+                s = function.Value(pointAndF.point) - pointAndF.f;
+                sum += s * s;
+            }
+            return sum;
+        }
+        public IVector Gradient(IFunction function)
+        {
+            Vector grad = new Vector();
+            IDifferentiableFunction fun;
+            if (function is IDifferentiableFunction)
+            {
+                fun = function as IDifferentiableFunction;
+
+                for (int i = 0; i < pointsAndF[0].point.Count; i++)
+                    grad.Add(0);
+                grad.Add(1);
+                for (int i = 0; i < pointsAndF.Count; i++)
+                {
+                    for (int j = 0; j <= pointsAndF[i].point.Count; j++)
+                        grad[j] += 2 * (fun.Value(pointsAndF[i].point) - pointsAndF[i].f) * fun.Gradient(pointsAndF[i].point)[j];
                 }
                 return grad;
             }
@@ -218,7 +254,7 @@ namespace TOOP_3sem
                 int i;
                 for (i = 0; i < MaxIter; i++)
                 {
-                    lambda = GoldenSelection(0, 2, 1e-14, obj, function, minparam);
+                    lambda = GoldenSelection(0, 2, 1e-9, obj, function, minparam);
 
                     for (int j = 0; j < param.Count; j++)
                         param[j] = minparam[j] - lambda * obj.Gradient(function.Bind(minparam))[j];
@@ -230,10 +266,10 @@ namespace TOOP_3sem
                     else
                         for (int j = 0; j < param.Count; j++)
                             minparam[j] = param[j];
-                    if (i == MaxIter - 1)
-                        Console.WriteLine("Maxiter! f = " + f.ToString());
+                    //if (i == MaxIter - 1)
+                    //    Console.WriteLine("Maxiter! f = " + f.ToString());
                 }
-                Console.WriteLine("Functional = " + f.ToString() + " Iter = " + i.ToString());
+                Console.WriteLine("Gradient: Functional = " + f.ToString() + " Iter = " + i.ToString());
             }
             return param;
         }
@@ -254,7 +290,7 @@ namespace TOOP_3sem
             int i;
             for (i = 0; i < MaxIter; i++)
             {
-                for (int j = 0; j < param.Count; j++) param[j] = rand.NextDouble();
+                for (int j = 0; j < param.Count; j++) param[j] = (rand.NextDouble() - 0.5) * 20;
                 var f = objective.Value(function.Bind(param));
                 if (f < currentmin)
                 {
@@ -262,16 +298,17 @@ namespace TOOP_3sem
                     for (int j = 0; j < param.Count; j++) minparam[j] = param[j];
                 }
             }
+            Console.WriteLine("MonteCarlo: Functional = " + currentmin.ToString());
+
             return minparam;
         }
     }
-
     class Program
     {
         static void Main(string[] args)
         {
-            //var optimizer = new MinimizerMonteCarlo();
-            var optimizer = new MinimizerGradient();
+            var optimizer = new MinimizerMonteCarlo();
+            //var optimizer = new MinimizerGradient();
             var initial = new Vector();
             Console.Write("Пространство: ");
             int n = int.Parse(Console.ReadLine());
@@ -298,7 +335,7 @@ namespace TOOP_3sem
                 points.Add((point, double.Parse(str.Last())));
             }
             //var functinal = new MyFunctional() { points = points };
-            var functinal = new MyL1NormDifferentiableFunctional() { pointsAndF = points };
+            var functinal = new MyL1Functional() { pointsAndF = points };
             
             //var fun = new LineFunction();
             var fun = new MyLineNFunction();
